b8a9999a196c9b62f494937e1b47c93560692384
Seongjun2 <tjdwns4302@naver.com>
2020-01-11 22:49:25 +0900

    LikedQueue 구현 완료

--- LinkedQueue.java
+++ LinkedQueue.java
@@ -1,4 +1,81 @@
 package DataStructure;
 
-public class LinkedQueue {
+public class LinkedQueue<T> implements Queue<T> {
+
+    private Node<T> start;
+    private int size;
+    private Node<T> end;
+
+    public LinkedQueue(){
+        start = null;
+        size = 0;
+    }
+    @Override
+    public void add(T input) {
+        Node<T> node = new Node(input, null);
+        if(size == 0){
+            start = node;
+            end = node;
+        }
+        else{
+            end.next = node;
+            end = node;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if(size == 0) return true;
+        else return false;
+    }
+
+    @Override
+    public boolean isFull() {// 노필요
+        return false;
+    }
+
+    @Override
+    public T poll() {
+        if(size == 0){
+            System.out.println("Queue is Empty");
+            return null;
+        }
+        else{
+            T value = start.value;
+            start = start.next;
+            size--;
+            return value;
+        }
+    }
+
+    @Override
+    public void clear() {
+        start = null;
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void resize() {//노 필
+
+    }
+
+    class Node<T>{
+        T value;
+        Node next;
+
+        public Node(){
+            value = null;
+            next = null;
+        }
+        public Node(T value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
 }
--- QueueTest.java
+++ QueueTest.java
@@ -2,33 +2,54 @@ package DataStructure;
 
 public class QueueTest {
     public static void main(String[] args) {
-        Queue<String> queue = new ArrayQueue<>();
+//        Queue<String> queue = new ArrayQueue<>();
+//
+//        queue.add("A");
+//        queue.add("B");
+//        queue.add("C");
+//        queue.add("D");
+//
+//
+//        System.out.println(queue.poll());
+//        System.out.println(queue.poll());
+//        System.out.println();
+//
+//        queue.add("A");
+//        queue.add("B");
+//
+//
+//        System.out.println(queue.poll());
+//        System.out.println(queue.poll());
+//        System.out.println(queue.poll());
+//        System.out.println(queue.poll());
+//        System.out.println();
+//
+//        queue.add("C");
+//        queue.add("D");
+//
+//        System.out.println(queue.poll());
+//        System.out.println(queue.poll());
 
-        queue.add("A");
-        queue.add("B");
-        queue.add("C");
-        queue.add("D");
+        /*---------------------------------------*/
 
+        Queue<String> q = new LinkedQueue<>();
+        q.add("A");
+        q.add("B");
+        q.add("C");
+        q.add("D");
 
-        System.out.println(queue.poll());
-        System.out.println(queue.poll());
-        System.out.println();
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
 
-        queue.add("A");
-        queue.add("B");
+        q.add("E");
+        q.add("F");
+        q.add("G");
 
-
-        System.out.println(queue.poll());
-        System.out.println(queue.poll());
-        System.out.println(queue.poll());
-        System.out.println(queue.poll());
-        System.out.println();
-
-        queue.add("C");
-        queue.add("D");
-
-        System.out.println(queue.poll());
-        System.out.println(queue.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
 
     }
 }
